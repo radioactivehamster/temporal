a68f81aac0610165d5fd40d75b9ef009e0cf4c71
James Benner <james.benner@gmail.com>
2016-02-17 11:29:14 -0500

    Refactored tests to be more DRY.

--- tests/Calendar/DaySpec.php
+++ tests/Calendar/DaySpec.php
@@ -10,6 +10,16 @@ use Temporal\Calendar\Day;
 
 class DaySpec extends ObjectBehavior
 {
+    /**
+     * Setup that runs before every test.
+     */
+    function let()
+    {
+        $unixEpoc = Carbon::createFromDate(1970, 1, 1, 'GMT');
+
+        $this->beConstructedWith($unixEpoc);
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType('Temporal\Calendar\Day');
@@ -17,64 +27,36 @@ class DaySpec extends ObjectBehavior
 
     function it_should_be_the_year_1970_on_the_unix_epoc()
     {
-        $day = Carbon::createFromDate(1970, 1, 1, 'GMT');
-
-        $this->beConstructedWith($day);
-        $this->inYear
-             ->shouldReturn(1970);
+        $this->inYear->shouldReturn(1970);
     }
 
     function it_should_be_january_on_the_unix_epoc()
     {
-        $day = Carbon::createFromDate(1970, 1, 1, 'GMT');
-
-        $this->beConstructedWith($day);
-        $this->month
-             ->shouldReturn('January');
+        $this->month->shouldReturn('January');
     }
 
     function it_should_be_thursday_on_the_unix_epoc()
     {
-        $day = Carbon::createFromDate(1970, 1, 1, 'GMT');
-
-        $this->beConstructedWith($day);
-        $this->name
-             ->shouldReturn('Thursday');
+        $this->name->shouldReturn('Thursday');
     }
 
     function it_should_be_the_first_month_of_the_year_on_the_unix_epoc()
     {
-        $day = Carbon::createFromDate(1970, 1, 1, 'GMT');
-
-        $this->beConstructedWith($day);
-        $this->ofMonth
-             ->shouldReturn(1);
+        $this->ofMonth->shouldReturn(1);
     }
 
     function it_should_be_the_fourth_week_of_the_year_on_the_unix_epoc()
     {
-        $day = Carbon::createFromDate(1970, 1, 1, 'GMT');
-
-        $this->beConstructedWith($day);
-        $this->ofWeek
-             ->shouldReturn(4);
+        $this->ofWeek->shouldReturn(4);
     }
 
     function it_should_be_the_correct_timestamp_on_the_unix_epoc()
     {
-        $day = Carbon::createFromDate(1970, 1, 1, 'GMT');
-
-        $this->beConstructedWith($day);
-        $this->timestamp
-             ->shouldReturn('1970-01-01T00:00:00+0000');
+        $this->timestamp->shouldReturn('1970-01-01T00:00:00+0000');
     }
 
     function it_should_be_set_to_the_gmt_timezone()
     {
-        $day = Carbon::createFromDate(1970, 1, 1, 'GMT');
-
-        $this->beConstructedWith($day);
-        $this->timezone
-             ->shouldReturn('GMT');
+        $this->timezone->shouldReturn('GMT');
     }
 }
