de94854f006b36f01c5457b4b9ba1609b1c080bc
James Benner <james.benner@gmail.com>
2016-02-16 21:25:11 -0500

    Added `it_should_be_thursday_on_the_first_day_of_the_unix_epoc()` test.

--- tests/Calendar/DaySpec.php
+++ tests/Calendar/DaySpec.php
@@ -2,13 +2,25 @@
 
 namespace tests\Temporal\Calendar;
 
+use Carbon\Carbon;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Temporal\Calendar;
+use Temporal\Calendar\Day;
 
 class DaySpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
         $this->shouldHaveType('Temporal\Calendar\Day');
+    }
+
+    function it_should_be_thursday_on_the_first_day_of_the_unix_epoc()
+    {
+        $day = Carbon::createFromDate(1970, 1, 1, 'GMT');
+
+        $this->beConstructedWith($day);
+        $this->name
+             ->shouldReturn('Thursday');
     }
 }
